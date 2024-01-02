010ebb3a6e46ad866dec2609b7f6e1f1dee0170c
dayeya <clipchamp.daniel@gmail.com>
2024-01-03 00:04:36 +0200

    listen error handling

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
-#[allow(unused_imports)]
-use std::net::{TcpListener, TcpStream};
+use std::net::{
+    TcpStream,
+    TcpListener,
+    SocketAddrV4, Ipv4Addr
+};
 
 #[allow(dead_code)]
-fn handle_stream() {
-    println!("Handling something...");
+fn handle_client(_stream: TcpStream) {
+    // ...
 }
 
+const PORT: u16 = 60101;
+const IP: Ipv4Addr = Ipv4Addr::new(127, 0, 0, 1);
+const ADDRESS: SocketAddrV4 = SocketAddrV4::new(IP, PORT);
+
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:0").unwrap();
+    let listener: TcpListener = TcpListener::bind(ADDRESS).unwrap();
+    if let Ok(addr) = listener.local_addr() {
+        println!("Irecv listening for connections at {}", addr);
+    } else {
+        panic!()
+    }
 }
