2033501afe1244fd2114c46a6c07f42a3eebaeed
dayeya <clipchamp.daniel@gmail.com>
2024-01-03 14:16:03 +0200

    Finished basic server setup.

--- src/main.rs
+++ src/main.rs
@@ -36,10 +36,6 @@ fn handle_client(mut stream: TcpStream) {
     }
 }
 
-const PORT: u16 = 60101;
-const IP: Ipv4Addr = Ipv4Addr::new(127, 0, 0, 1);
-const ADDRESS: SocketAddrV4 = SocketAddrV4::new(IP, PORT);
-
 fn main() {
     let listener: TcpListener = TcpListener::bind(ADDRESS).expect(
     &format!("ERROR: Could not start Irecv at {}", ADDRESS)
