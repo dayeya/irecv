0a04c885acd6c37667e75dbbe29338836649c820
dayeya <clipchamp.daniel@gmail.com>
2024-01-08 21:06:02 +0200

    Simple chat working

--- src/main.rs
+++ src/main.rs
@@ -1,90 +1,113 @@
 use core::fmt;
-use std::collections::HashMap;
-// use std::collections::HashMap;
+// use std::ops::Deref;
+// use std::sync::Arc;
 use std::thread;
+use std::collections::HashMap;
 use std::io::{Read, Write};
-// use std::sync::{Arc, Mutex};
-// use std::collections::HashMap;
 use std::sync::mpsc::{Sender, Receiver, channel};
 use std::net::{Shutdown, TcpStream, TcpListener, SocketAddr, SocketAddrV4};
 
 const PORT: u16 = 60101;
 const IP: &str = "192.168.1.218";
 const BUFFER: usize = 1024;
+
+struct Client {
+    conn: TcpStream,
+    message_count: u32,
+}
+
+impl Client {
+    fn from_stream(s: TcpStream) -> Self {
+        Client {
+            conn: s,
+            message_count: 0,
+        }
+    }
+
+    fn peer_addr(&self) -> SocketAddr {
+        self.conn.peer_addr().unwrap()
+    }
+}
+
 enum Message {
-    // NewConnection,
-    // ClientAborted,
-    Regular(Vec<u8>)
+    NewConnection(TcpStream),
+    ClientAborted(TcpStream),
+    Regular(Vec<u8>, SocketAddr)
 }
 
 impl fmt::Display for Message {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            // Message::NewConnection => Ok(()), 
-            // Message::ClientAborted => Ok(()), 
-            Message::Regular(buffer) => write!(f, "{:?}", buffer),
+            Message::NewConnection(_) => Ok(()), 
+            Message::ClientAborted(_) => Ok(()), 
+            Message::Regular(buffer, _) => write!(f, "{:?}", buffer),
         }
     }
 }
 
-struct Client {
-    addr: SocketAddr,
-}
-
-struct Server {
-    clients: HashMap<Client, TcpStream>,
-}
-
 fn server(messages: Receiver<Message>) -> Result<(), ()> {
+    let mut clients = HashMap::<SocketAddr, Client>::new(); 
     loop {
         let msg: Message = messages.recv().expect("The receiver is not up!");
         match msg {
-            // Message::NewConnection => {println!("A new client connected!")},
-            // Message::ClientAborted => {println!("Oof, a client disconnected...")},
-            Message::Regular(data) => {
-                println!("A client sent: {:?}", data)
+            Message::NewConnection(stream) => {
+                let current_addr: SocketAddr = stream.peer_addr().unwrap();
+                clients.insert(current_addr, Client::from_stream(stream));
+            },
+            Message::ClientAborted(stream) => {
+                let current_addr = stream.peer_addr().unwrap();
+                clients.remove(&current_addr);
+            },
+            Message::Regular(data, sender) => {
+                for (addr, client) in clients.iter_mut() {
+                    if *addr != sender {
+                        let _ = client.conn.write(&data).map_err(
+                            |e| eprint!("ERROR: Could not broadcast to {}, {e}", client.peer_addr())
+                        );
+                    }
+                }
             },
         }
     }
 }
 
-
 fn handle_client(mut stream: TcpStream, messages: Sender<Message>) -> Result<(), ()> {
-    stream.write(&"Hello, Welcome to the Irecv!\n".as_bytes()).map_err(
+    stream.write(&"Hello, Welcome to Irecv!\n".as_bytes()).map_err(
         |e| eprintln!("Could not greet {}, {e}", stream.local_addr().unwrap())
     )?;
-    // messages.send(Message::NewConnection).map_err(
-    //     |e| eprintln!("ERROR: Could not receive a new connection, {}", e)
-    // )?;
+    let mut client: Client = Client::from_stream(stream);
+    messages.send(Message::NewConnection(client.conn.try_clone().unwrap())).map_err(
+        |e| eprintln!("ERROR: Could not send a new connection, {}", e)
+    )?;
     let mut buff: Vec<u8> = Vec::new();
     buff.resize(BUFFER, 0);
     loop {
-        match stream.read(&mut buff) {
+        match client.conn.read(&mut buff) {
             Ok(n) => {
-                messages.send(Message::Regular(buff[..n].to_vec())).map_err(
+                let data = buff[..n].to_vec();
+                messages.send(Message::Regular(data, client.peer_addr())).map_err(
                     |e| eprintln!("ERROR: Could not send to receiver, {}", e)
                 )?;
             },
             Err(e) => {
-                eprintln!("ERROR failed to read data from {} due to: {e}", stream.local_addr().unwrap());
-                if let Err(e) = stream.shutdown(Shutdown::Both) {
-                    eprintln!("ERROR: Failed shutdown at {:?}, {e}", stream.local_addr().unwrap());
+                eprintln!("ERROR failed to read data from {} due to: {e}", client.peer_addr());
+                if let Err(e) = client.conn.shutdown(Shutdown::Both) {
+                    eprintln!("ERROR: Failed shutdown at {:?}, {e}", client.peer_addr());
                 }
             }
         }
     }
 }
 
-
 fn main() {
-    let address: SocketAddrV4 = format!("{IP}:{PORT}").parse().unwrap();
+    let address = format!("{IP}:{PORT}").parse::<SocketAddrV4>().unwrap();
     let listener: TcpListener = TcpListener::bind(address).expect(
         &format!("ERROR: Could not start Irecv at {address}")
     );
     println!("INFO: Irecv listening for connections at {}", address);
 
-    let (sender, receive) = channel::<Message>();
-    thread::spawn(|| server(receive));
+    let (sender, receiver) = channel::<Message>();
+    thread::spawn(|| server(receiver));
     
     loop  {
         match listener.accept() {
